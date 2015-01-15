674465cab96fbeaa4b94fb8e49ee290992e93352
Pieter Heyvaert <pheyvaer.heyvaert@ugent.be>
2015-01-15 16:45:43 +0100

    added starter code for generating the custom catalog

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 var loader = require('./lib/loader.js');
 var writer = require('./lib/writer.js');
+var cataloger = require('./lib/cataloger.js');
+var N3 = require('n3');
 
 var outputFile;
 
 process.argv.forEach(function (val, index, array) {
   	if (index === 2) {
 		outputFile = val;
+		console.log(outputFile);
 	}
 });
 
 loader.load().then(function(store){
-	writer.write(store, outputFile);
+	var cStore = cataloger.catalog(store);
+
+//	allTriples = cStore.find(null, null, null);
+
+//	console.log(allTriples.length);
+//	allTriples.forEach(function(triple){
+//		console.log(triple.subject);
+//	});
+
+	writer.write(cStore, outputFile);
 });
