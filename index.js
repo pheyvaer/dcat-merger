005c010aef9ab9db7acff2343083d81b12e0e34e
Pieter Heyvaert <pheyvaer.heyvaert@ugent.be>
2015-01-22 10:43:56 +0100

    fix indents, update prints

--- index.js
+++ index.js
@@ -10,62 +10,67 @@ var outputFile;
 var outputFileIndex;
 var errorMessage = "Incorrect parameter usage.";
 
-var badExit = function(){
-    console.log(errorMessage);
-    process.exit(1);
+var badExit = function() {
+  console.log(errorMessage);
+  process.exit(1);
 }
 
 
-process.argv.forEach(function (val, index, array) {
-	if (val === "-o") {
-		if (outputFileIndex || index == array.length - 1) {
-			badExit();
-		} else {
-			outputFileIndex = index + 1;
-		}
-	} 
+process.argv.forEach(function(val, index, array) {
+  if (val === "-o") {
+    if (outputFileIndex || index == array.length - 1) {
+      badExit();
+    } else {
+      outputFileIndex = index + 1;
+    }
+  }
 
-	if (val === "-v") {
-		global.verbose = true;
-	}
+  if (val === "-v") {
+    global.verbose = true;
+  }
 
-  	if (index === outputFileIndex) {
-		if (val === "-o" || val === "-v") {
-			badExit();
-		}
+  if (index === outputFileIndex) {
+    if (val === "-o" || val === "-v") {
+      badExit();
+    }
 
-		outputFile = val;
-	}
+    outputFile = val;
+  }
 });
 
 console.log("Loading & parsing started ");
-loader.load().then(function(store){
-	console.log("Loading & parsing finished");
+loader.load().then(function(store) {
+  console.log("Loading & parsing finished");
 
-	if (global.verbose) {
-		console.log("Cataloging started");
-	} else {
-		process.stdout.write("Cataloging ... ");
-	}
-	cataloger.catalog(store, verbose).then(function(cStore){
-		if (global.verbose) {
-			console.log("Cataloging finished");
-		} else {
-			console.log("done");
-		}
+  if (global.verbose) {
+    console.log("Cataloging started");
+  } else {
+    process.stdout.write("Cataloging ... ");
+  }
 
-		if (global.verbose) {
-			console.log("Writing to '" + outputFile + "' started");
-		} else {
-			process.stdout.write("Writing to '" + outputFile + "' ... ");
-		}
+  cataloger.catalog(store, verbose).then(function(cStore) {
+    if (global.verbose) {
+      console.log("Cataloging finished");
+    } else {
+      console.log("done");
+    }
 
-		writer.write(cStore, outputFile);
+    if (outputFile) {
+      if (global.verbose) {
+        console.log("Writing to '" + outputFile + "' started");
+      } else {
+        process.stdout.write("Writing to '" + outputFile + "' ... ");
+      }
+    }
 
-		if (global.verbose) {
-			console.log("Writing to '" + outputFile + "' finished");
-		} else {
-			console.log("done");
-		}
-	});
+    writer.write(cStore, outputFile);
+
+    if (outputFile) {
+      if (global.verbose) {
+        console.log("Writing to '" + outputFile + "' finished");
+      } else {
+        console.log("done");
+      }
+    }
+  });
 });
